1de18360a5af063a5a2028ff6a4717249da35c36
FrzAdli <adlifarizi04@gmail.com>
2024-06-15 17:01:33 +0700

    fix shortUrl logic

--- functions/server.js
+++ functions/server.js
@@ -8,7 +8,6 @@ const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const app = express();
-
 const router = express.Router();
 
 // Middleware
@@ -82,119 +81,116 @@ router.post('/shorten', async (req, res) => {
 
 // Endpoint untuk mengakses URL pendek
 router.get('/:shortUrl', async (req, res) => {
+    const { shortUrl } = req.params;
+
     try {
-        const { shortUrl } = req.params;
         const urlSnapshot = await urlsCollection.where('shortUrl', '==', shortUrl).get();
 
         if (urlSnapshot.empty) {
             console.log(`Received request for short URL: ${shortUrl}`);
             console.log(`Query result for ${shortUrl}:`, urlSnapshot);
-            // return res.status(404).sendFile(path.join(__dirname, '..', 'public', '404.html'));
+            return res.status(404).sendFile(path.join(__dirname, '..', 'public', '404.html'));
         }
+
+        const urlDoc = urlSnapshot.docs[0];
+        const urlData = urlDoc.data();
+
+        // Cek apakah URL sudah kedaluwarsa
+        if (urlData.expireDate && new Date() > urlData.expireDate.toDate()) {
+            // Hapus dokumen yang sudah kadaluwarsa
+            await urlDoc.ref.delete();
+            return res.status(410).sendFile(path.join(__dirname, '..', 'public', 'expired.html'));
+        }
+
+        // Jika URL memiliki password
+        if (urlData.password) {
+            // Jika pengguna belum memasukkan password
+            if (!req.query.password) {
+                // Tampilkan halaman memasukkan password
+                return res.sendFile(path.join(__dirname, '..', 'public', 'password.html'));
+            }
+
+            // Verifikasi password yang dimasukkan pengguna
+            const isPasswordValid = await bcrypt.compare(req.query.password, urlData.password);
+            if (isPasswordValid) {
+                // Redirect ke original URL jika password valid
+                return res.redirect(urlData.originalUrl);
+            } else {
+                // Jika password salah, tampilkan pesan kesalahan di halaman password
+                return res.send(`
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Enter Password</title>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                                margin: 0;
+                                padding: 20px;
+                            }
+                            .container {
+                                max-width: 400px;
+                                margin: 0 auto;
+                                text-align: center;
+                            }
+                            h2 {
+                                margin-bottom: 20px;
+                            }
+                            form {
+                                display: flex;
+                                flex-direction: column;
+                                align-items: center;
+                            }
+                            label {
+                                margin-bottom: 10px;
+                            }
+                            input[type="password"] {
+                                width: 100%;
+                                padding: 10px;
+                                margin-bottom: 20px;
+                                border: 1px solid #ccc;
+                                border-radius: 5px;
+                            }
+                            button {
+                                padding: 10px 20px;
+                                background-color: #007bff;
+                                color: #fff;
+                                border: none;
+                                border-radius: 5px;
+                                cursor: pointer;
+                            }
+                            button:hover {
+                                background-color: #0056b3;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <div class="container">
+                            <h2>Masukkan Kata Sandi</h2>
+                            <h5>Kamu butuh kata sandi untuk melanjutkan</h5>
+                            <form method="GET" action="">
+                                <label for="password">Kata Sandi:</label>
+                                <input type="password" id="password" name="password" required>
+                                <div id="error-message" style="color: red;">Kata sandi tidak sesuai</div>
+                                <button type="submit">Submit</button>
+                            </form>
+                        </div>
+                    </body>
+                    </html>
+                `);
+            }
+        }
+
+        // Jika URL tidak memiliki password, langsung redirect ke original URL
+        res.redirect(urlData.originalUrl);
     } catch (error) {
         console.error('Error in /:shortUrl endpoint:', error);
+        res.status(500).send('Internal Server Error');
     }
-    
-    const urlDoc = urlSnapshot.docs[0];
-    const urlData = urlDoc.data();
-
-    // Cek apakah URL sudah kedaluwarsa
-    if (urlData.expireDate && new Date() > urlData.expireDate.toDate()) {
-        // Hapus dokumen yang sudah kadaluwarsa
-        await urlDoc.ref.delete();
-        return res.status(410).sendFile(path.join(__dirname, '..', 'public', 'expired.html'));
-    }
-
-    // Jika URL memiliki password
-    if (urlData.password) {
-        // Jika pengguna belum memasukkan password
-        if (!req.query.password) {
-            // Tampilkan halaman memasukkan password
-            return res.sendFile(path.join(__dirname, '..', 'public', 'password.html'));
-        }
-
-        // Verifikasi password yang dimasukkan pengguna
-        const isPasswordValid = await bcrypt.compare(req.query.password, urlData.password);
-        if (isPasswordValid) {
-            // Redirect ke original URL jika password valid
-            return res.redirect(urlData.originalUrl);
-        } else {
-            // Jika password salah, tampilkan pesan kesalahan di halaman password
-            return res.send(`
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Enter Password</title>
-                    <style>
-                        body {
-                            font-family: Arial, sans-serif;
-                            margin: 0;
-                            padding: 20px;
-                        }
-                        .container {
-                            max-width: 400px;
-                            margin: 0 auto;
-                            text-align: center;
-                        }
-                        h2 {
-                            margin-bottom: 20px;
-                        }
-                        form {
-                            display: flex;
-                            flex-direction: column;
-                            align-items: center;
-                        }
-                        label {
-                            margin-bottom: 10px;
-                        }
-                        input[type="password"] {
-                            width: 100%;
-                            padding: 10px;
-                            margin-bottom: 20px;
-                            border: 1px solid #ccc;
-                            border-radius: 5px;
-                        }
-                        button {
-                            padding: 10px 20px;
-                            background-color: #007bff;
-                            color: #fff;
-                            border: none;
-                            border-radius: 5px;
-                            cursor: pointer;
-                        }
-                        button:hover {
-                            background-color: #0056b3;
-                        }
-                    </style>
-                </head>
-                <body>
-                    <div class="container">
-                        <h2>Masukkan Kata Sandi</h2>
-                        <h5>Kamu butuh kata sandi untuk melanjutkan</h5>
-                        <form method="GET" action="">
-                            <label for="password">Kata Sandi:</label>
-                            <input type="password" id="password" name="password" required>
-                            <div id="error-message" style="color: red;">Kata sandi tidak sesuai</div>
-                            <button type="submit">Submit</button>
-                        </form>
-                    </div>
-                </body>
-                </html>
-            `);
-        }
-    }
-
-    // Jika URL tidak memiliki password, langsung redirect ke original URL
-    res.redirect(urlData.originalUrl);
 });
 
-app.use('/.netlify/functions/server', router)
-
-// Netlify mendukung otomatis, jadi tidak perlu menentukan port
-// app.listen(3000, () => {
-    
-// });
+app.use('/.netlify/functions/server', router);
 
 module.exports.handler = serverless(app);
