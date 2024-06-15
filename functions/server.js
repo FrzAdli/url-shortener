23486d28a9f3aa520924d4e53787e31245205372
FrzAdli <adlifarizi04@gmail.com>
2024-06-15 21:01:46 +0700

    fixing errors

--- functions/server.js
+++ functions/server.js
@@ -12,9 +12,9 @@ const app = express();
 const router = express.Router();
 
 // Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Menyediakan file statis
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public'))); 
 
 // Konfigurasi Firebase Admin SDK
 const serviceAccount = {
